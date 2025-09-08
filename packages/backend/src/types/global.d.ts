38f20690f52101585f2259e3e6dc06c022f7e12b
Kevin <kevin@despoke.io>
2025-09-08 19:54:48 -0400

    updated globals

--- packages/backend/src/types/global.d.ts
+++ packages/backend/src/types/global.d.ts
@@ -3,6 +3,22 @@ import 'express';
 
 declare global {
   namespace Express {
+    interface Request {
+      params: any;
+      body: any;
+      query: any;
+      headers: any;
+      ip: string;
+      hostname: string;
+      path: string;
+      url: string;
+      method: string;
+      files?: any;
+      file?: any;
+      get: (name: string) => string | string[] | undefined;
+      code?: any;
+    }
+    
     namespace Multer {
       interface File {
         fieldname: string;
@@ -16,6 +32,25 @@ declare global {
         buffer: Buffer;
       }
     }
+  }
+}
+
+// Extend the base Express Request interface
+declare module 'express-serve-static-core' {
+  interface Request {
+    params: any;
+    body: any;
+    query: any;
+    headers: any;
+    ip: string;
+    hostname: string;
+    path: string;
+    url: string;
+    method: string;
+    files?: any;
+    file?: any;
+    get: (name: string) => string | string[] | undefined;
+    code?: any;
   }
 }
 
